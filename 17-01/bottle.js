0b79e2deb21da30fea32169244dbbddf3fa87817
hiteshsubnani0128 <hiteshsubnani734@gmail.com>
2019-01-17 22:09:59 +0530

    17-01 added

---
+++ 17-01/bottle.js
@@ -0,0 +1,6 @@
+i = 99;
+while(i!==0){
+console.log(`${i} bottles of beer on the wall, ${i} bottles of beer`);
+i--;
+console.log(`Take one down and pass it around, ${i} bottles of beer on the wall`);
+}
